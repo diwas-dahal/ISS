240e6f5b943338192ae6bf246436d7b2027b7ed9
diwas-dahal <diwasdahal08@gmail.com>
2021-10-29 17:27:05 +0545

    blue print of object in OneTimePad.py

--- OneTimePad.py
+++ OneTimePad.py
@@ -1,1 +1,18 @@
-"" 
+import sys
+import random
+
+
+class OneTimePad(object):
+    def __init__(self, filePath, encrypt=True,  extension="text"):
+        self.filePath = filePath
+        self.encrypt = encrypt
+        self.extension = extension
+
+    def __str__(self):
+        return f"{self.filePath}  {self.encrypt}"
+
+    def Encrypt(self, key):
+        return "Encrypting File"
+
+    def Decrypt(self, key):
+        return "Decrypting File"
